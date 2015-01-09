a8fd498a5f9719f67a19bcd774de000cca379a2f
WirelessCaveman <wireless.caveman@gmail.com>
2015-01-09 08:42:35 -0800

    Fixed bug in recursive Binary Search

--- BinarySearch/src/binsearch.c
+++ BinarySearch/src/binsearch.c
@@ -4,7 +4,7 @@
  Author      : 
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : Binary search
  ============================================================================
  */
 
@@ -38,31 +38,33 @@ int binsearch_rec (int arr[], int find, int len)
 {
 	int start = 0, end = len - 1;
 	static int found = -1;
-	if (start <= end)
+	if (start < end)
 	{
-		int mid = end / 2;
+		int mid = len / 2;
 		if (arr[mid] == find)
 		{
 			found = mid;
 			return found;
 		}
-		else if (arr[mid] < find)
-		{
-			binsearch_rec(&(arr[mid]), find, len-mid);
-		}
-		else
+		else if (find < arr[mid])
 		{
 			binsearch_rec(&(arr[0]), find, mid);
 		}
+		else
+		{
+			binsearch_rec(&(arr[mid]), find, len-mid);
+			if (found != -1) found += mid;
+		}
 	}
+	else if ((start >= end) && (arr[end] == find)) found = end;
 	return found;
 }
 
 int main(void)
 {
 	int array[7] = {-3, 4, 4, 6, 21, 22, 22};
-//	printf("found %d at %d\n",22, binsearch(array, 22, sizeof(array)/sizeof(array[0])));
-	printf("NonRec found %d at %d\n",4 , binsearch_nonrec(array, 4, 7));
-	printf("Recursive found %d at %d\n",4 , binsearch_rec(array, 4, 7));
+	int find  = 7;
+	printf("NonRec found %d at %d\n", find, binsearch_nonrec(array, find, 7));
+	printf("Recursive found %d at %d\n", find, binsearch_rec(array, find, 7));
 	return EXIT_SUCCESS;
 }
